f82a81023269cf50c0067d0a3608ce1912fa0005
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-08-16 09:39:51 +0200

    update to node-red 0.19

--- 80-function.js
+++ 80-function.js
@@ -62,9 +62,13 @@ module.exports = function(RED) {
                            "results = (function(msg){ "+
                               "var __msgid__ = msg._msgid;"+
                               "var node = {"+
+                                 "id:__node__.id,"+
+                                 "name:__node__.name,"+
                                  "log:__node__.log,"+
                                  "error:__node__.error,"+
                                  "warn:__node__.warn,"+
+                                 "debug:__node__.debug,"+
+                                 "trace:__node__.trace,"+
                                  "on:__node__.on,"+
                                  "status:__node__.status,"+
                                  "send:function(msgs){ __node__.send(__msgid__,msgs);}"+
@@ -83,6 +87,8 @@ module.exports = function(RED) {
                 util: RED.util
             },
             __node__: {
+                id: node.id,
+                name: node.name,
                 log: function() {
                     node.log.apply(node, arguments);
                 },
@@ -91,6 +97,12 @@ module.exports = function(RED) {
                 },
                 warn: function() {
                     node.warn.apply(node, arguments);
+                },
+                debug: function() {
+                    node.debug.apply(node, arguments);
+                },
+                trace: function() {
+                    node.trace.apply(node, arguments);
                 },
                 send: function(id, msgs) {
                     sendResults(node, id, msgs);
@@ -133,7 +145,17 @@ module.exports = function(RED) {
                     return node.context().flow.keys.apply(node,arguments);
                 }
             },
-            global:RED.settings.functionGlobalContext || {},
+            global: {
+                set: function() {
+                    node.context().global.set.apply(node,arguments);
+                },
+                get: function() {
+                    return node.context().global.get.apply(node,arguments);
+                },
+                keys: function() {
+                    return node.context().global.keys.apply(node,arguments);
+                }
+            },
             setTimeout: function () {
                 var func = arguments[0];
                 var timerId;
@@ -178,9 +200,23 @@ module.exports = function(RED) {
                 }
             }
         };
+        if (util.hasOwnProperty('promisify')) {
+            sandbox.setTimeout[util.promisify.custom] = function(after, value) {
+                return new Promise(function(resolve, reject) {
+                    sandbox.setTimeout(function(){ resolve(value) }, after);
+                });
+            }
+        }
         var context = vm.createContext(sandbox);
         try {
-            this.script = vm.createScript(functionText);
+            this.script = vm.createScript(functionText, {
+                filename: 'Function node:'+this.id+(this.name?' ['+this.name+']':''), // filename for stack traces
+                displayErrors: true
+                // Using the following options causes node 4/6 to not include the line number
+                // in the stack output. So don't use them.
+                // lineOffset: -11, // line number offset to be used for stack traces
+                // columnOffset: 0, // column number offset to be used for stack traces
+            });
             this.on("input", function(msg) {
                 try {
                     var start = process.hrtime();
@@ -195,6 +231,13 @@ module.exports = function(RED) {
                         this.status({fill:"yellow",shape:"dot",text:""+converted});
                     }
                 } catch(err) {
+                    //remove unwanted part
+                    var index = err.stack.search(/\n\s*at ContextifyScript.Script.runInContext/);
+                    err.stack = err.stack.slice(0, index).split('\n').slice(0,-1).join('\n');
+                    var stack = err.stack.split(/\r?\n/);
+
+                    //store the error in msg to be used in flows
+                    msg.error = err;
 
                     var line = 0;
                     var errorMessage;
